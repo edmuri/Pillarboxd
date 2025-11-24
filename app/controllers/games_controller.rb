c4cff889b1b8aa0c9fe37e18b38b3ceea95d2906
Martin Gawron <martingaw11@gmail.com>
2025-11-23 18:23:04 -0600

    added navbar to layout and searching for games library

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,15 @@
 class GamesController < ApplicationController
   def index
-    @games = Game.order(
-      Arel.sql("CASE WHEN cover_url = 'NO COVER' THEN 1 ELSE 0 END"),
+    @games = Game.all
+
+    # apply filter if it exists
+    if params[:query].present?
+      @games = @games.where("title LIKE ?", "%#{params[:query]}%")
+    end
+
+    # order games: those without cover first, then alphabetically by title
+    @games = @games.order(
+      Arel.sql("CASE WHEN cover_url = 'NO COVER' THEN 1 ELSE 0 END"), 
       :title
     )
   end
