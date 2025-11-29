fa8dc1449db5a07b8f9cd4c222c2510a717c2e16
Eddie <ed.murillo717@gmail.com>
2025-11-28 22:08:35 -0600

    fixing log access for nonusers

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,24 +1,26 @@
 class LogsController < ApplicationController
     def index
+        if current_user
+            @all_logged = Game.left_joins(:logs)
+                        .select("games.title, games.cover_url, games.game_id, logs.log_id, logs.play_date")
+                        .where("user_id = ?", current_user.id)
 
-        @all_logged = Game.left_joins(:logs)
-                    .select("games.title, games.cover_url, games.game_id, logs.log_id, logs.play_date")
-                    .where("user_id = ?", current_user.id)
+            if params[:query].present?
+                @logged = @all_logged.where("title LIKE ?", "%#{params[:query]}%")
+            else    
+                @logged = @all_logged
+            end
 
-        if params[:query].present?
-            @logged = @all_logged.where("title LIKE ?", "%#{params[:query]}%")
-        else    
-            @logged = @all_logged
+            @logged = @logged.order(
+            Arel.sql("CASE WHEN cover_url = 'NO COVER' THEN 1 ELSE 0 END"), 
+            play_date: :desc
+            )
+
+            @logged = @logged.page(params[:page])
+                    .per(50)
+        else
+            @logged = Log.none
         end
-
-        @logged = @logged.order(
-        Arel.sql("CASE WHEN cover_url = 'NO COVER' THEN 1 ELSE 0 END"), 
-        play_date: :desc
-        )
-
-        @logged = @logged.page(params[:page])
-                   .per(50)
-
     end
     def show
         @game = Game.includes(:developers, :publishers, :genres, :platforms).find(params[:id])
